246808502f958fcc8a9f1a509c544c588e63efbb
garethdavies01bbc <gareth.davies.01@bbc.co.uk>
2020-08-20 16:11:18 +0100

    Fixed memchache tests

--- src/main/java/bbc/forge/dsp/jaxrs/MemcacheStatusFilter.java
+++ src/main/java/bbc/forge/dsp/jaxrs/MemcacheStatusFilter.java
@@ -1,17 +1,12 @@
 package bbc.forge.dsp.jaxrs;
 
 import bbc.forge.dsp.common.AsynchronousListenerWrapper;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 
-/**
- * User: iviec01
- * Date: 01/02/2012
- * Time: 10:24
- */
 public class MemcacheStatusFilter implements ContainerResponseFilter {
-
     private AsynchronousListenerWrapper asynchronousListenerWrapper;
 
     @Override
@@ -19,10 +14,8 @@ public class MemcacheStatusFilter implements ContainerResponseFilter {
 
         if (!asynchronousListenerWrapper.isServiceDependencyWorking()) {
             System.out.println("Starting status code:" + containerResponseContext.getStatus());
-
             containerResponseContext.setStatus(500);
             containerResponseContext.setEntity("Memcached is down...");
-
             System.out.println("Ending status code:" + containerResponseContext.getStatus());
         }
     }
--- src/test/java/bbc/forge/dsp/jaxrs/MemcacheStatusFilterTest.java
+++ src/test/java/bbc/forge/dsp/jaxrs/MemcacheStatusFilterTest.java
@@ -1,15 +1,10 @@
 package bbc.forge.dsp.jaxrs;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 
-import org.apache.cxf.jaxrs.model.ClassResourceInfo;
-import org.apache.cxf.message.Message;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,11 +13,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import bbc.forge.dsp.common.AsynchronousListenerWrapper;
 
-/**
- * User: iviec01
- * Date: 01/02/2012
- * Time: 10:25
- */
 @RunWith(MockitoJUnitRunner.class)
 public class MemcacheStatusFilterTest {
 
@@ -30,50 +20,35 @@ public class MemcacheStatusFilterTest {
     @Mock
     private AsynchronousListenerWrapper mockMemcachedListener;
     @Mock
-    private Message mockMessage;
-    @Mock
-    private ClassResourceInfo mockClassResourceInfo;
-    @Mock
     private ContainerResponseContext mockResponse;
+    @Mock
+    private ContainerRequestContext mockRequest;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         memcacheStatusFilter = new MemcacheStatusFilter();
         memcacheStatusFilter.setAsynchronousListenerWrapper(mockMemcachedListener);
     }
 
     @Test
-    public void handleRequestShouldReturnNullIfMemcacheIsUp() {
-
+    public void filterShouldNotSetStatusAndEntityIfMemcacheIsUp() {
         when(mockMemcachedListener.isServiceDependencyWorking()).thenReturn(true);
 
-        memcacheStatusFilter.filter(null, mockResponse);
+        memcacheStatusFilter.filter(mockRequest, mockResponse);
 
         verify(mockMemcachedListener).isServiceDependencyWorking();
-        assertNull(mockResponse);
+        verify(mockResponse, never()).setStatus(anyInt());
+        verify(mockResponse, never()).setEntity(anyString());
     }
 
     @Test
-    public void handleRequestShouldReturnAResponseMessageIfMemcacheIsDown () {
+    public void filterShouldSetStatusAndEntityCorrectlyIfMemcacheIDown() {
         when(mockMemcachedListener.isServiceDependencyWorking()).thenReturn(false);
 
-        memcacheStatusFilter.filter(null, mockResponse);
+        memcacheStatusFilter.filter(mockRequest, mockResponse);
 
         verify(mockMemcachedListener).isServiceDependencyWorking();
-
-        assertNotNull(mockResponse);
-        assertEquals("Memcached is down...", mockResponse.getEntity().toString());
-    }
-
-    @Test
-    public void handleRequestShouldReturnA500ResponseCodeIfMemcacheIsDown () {
-        when(mockMemcachedListener.isServiceDependencyWorking()).thenReturn(false);
-
-        memcacheStatusFilter.filter(null, mockResponse);
-
-        verify(mockMemcachedListener).isServiceDependencyWorking();
-
-        assertNotNull(mockResponse);
-        assertEquals(500, mockResponse.getStatus());
+        verify(mockResponse).setStatus(500);
+        verify(mockResponse).setEntity("Memcached is down...");
     }
 }
